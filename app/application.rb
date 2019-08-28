a9915e6f81ce5bc4b42e4d59168aa5743d22d966
Warren Yao <warrenyca905@gmail.com>
2019-08-28 01:18:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      @@items.find{|i| i.name == item_name}
+      item = @@items.find{|i| i.name == item_name}
       
       resp.write 
 end
