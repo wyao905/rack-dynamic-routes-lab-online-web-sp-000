219ef593313d50b1ddf991e530e6e14b7bcaea46
Warren Yao <warrenyca905@gmail.com>
2019-08-28 01:20:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,5 +9,6 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       
-      resp.write 
+      resp.write item.price
+    end
 end
