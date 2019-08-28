db370c459826cac35083a640e30e81f79f5a5148
Warren Yao <warrenyca905@gmail.com>
2019-08-28 01:30:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       
-      if !item
+      if item
         resp.write item.price
       else
         resp.status = 400
