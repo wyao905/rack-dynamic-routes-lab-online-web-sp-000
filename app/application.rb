07ca525962a552843969368f3b912264027cbd37
Warren Yao <warrenyca905@gmail.com>
2019-08-28 01:30:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,9 +10,13 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      binding.pry
       
-      resp.write item.price
+      if !item
+        resp.write item.price
+      else
+        resp.status = 400
+      end
+      
     else
       resp.write "Route not found"
       resp.status = 404
